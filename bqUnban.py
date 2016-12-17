c1b44d0d89658dd4997e9690c91a555d5d8bdbc5
Ignas <Hoffs-@users.noreply.github.com>
2016-12-17 22:41:25 +0200

    Updating script
    Random fixes

--- bqUnban.py
+++ bqUnban.py
@@ -8,7 +8,7 @@ import time
 
 # screen -S M -X stuff "pardon user ^M"
 
-ITERATIONS_BETWEEN_MESSAGES = 5
+ITERATIONS_BETWEEN_MESSAGES = 8
 TIME_TO_SLEEP = 60
 LIVES_TO_GIVE = 2
 MESSAGE_FILE = "serverMessages.txt"
@@ -62,7 +62,6 @@ class NbtEditor():
 
 
 class BannedPurger():
-    bDict = dict()
     logFile = "unbanLog.log"
 
     def __init__(self):
@@ -71,7 +70,8 @@ class BannedPurger():
         self.nbtManager = NbtEditor()
 
     def parseBanned(self):
-        self.logWriter.write("[{1}][parseBanned] Parsing banned JSON. Current dictionary {0}.\n".format(BannedPurger.bDict, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+        self.bDict = dict()
+        self.logWriter.write("[{1}][parseBanned] Parsing banned JSON. Current dictionary {0}.\n".format(self.bDict, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
         fileLocation = "banned-players.json"
         f = open(fileLocation, 'r')
         fileContents = f.read()
@@ -83,25 +83,26 @@ class BannedPurger():
             timeString = x['created']
             timeString = int(time.mktime((datetime.strptime(timeString, "%Y-%m-%d %H:%M:%S %z")).timetuple()))
             if x['reason'] == "Death in Hardcore":
-                BannedPurger.bDict[name]=[uuid, timeString]
+                self.bDict[name]=[uuid, timeString]
                 self.logWriter.write("[{3}][parseBanned] Adding to dictionary: {0} {1} {2}.\n".format(name, uuid, timeString, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
 
 
     def checkUsers(self):
         self.logWriter.write("[{0}][checkUsers] Checking for bans.\n".format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
-        for x in BannedPurger.bDict:
-            uuid = BannedPurger.bDict[x][0]
-            etime = BannedPurger.bDict[x][1]
+        for x in self.bDict:
+            uuid = self.bDict[x][0]
+            etime = self.bDict[x][1]
             self.logWriter.write("[{3}][checkUsers] Checking user {0} with UUID {1}. Banned time {2}, current time {4}.\n".format(x, uuid, etime, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), int(calendar.timegm(time.gmtime()))))
-            if int(calendar.timegm(time.gmtime())) - BannedPurger.bDict[x][1] > BANNED_TIME:
+            if int(calendar.timegm(time.gmtime())) - self.bDict[x][1] > BANNED_TIME:
                 self.logWriter.write("[{3}][checkUsers] Removing ban for {0} with UUID {1}.\n".format(x, uuid, etime, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
-                print(BannedPurger.bDict[x][0],BannedPurger.bDict[x][1])
+                print(self.bDict[x][0], self.bDict[x][1])
                 self.nbtManager.addLives(uuid, self.logWriter)
                 unbanString = 'screen -S M -X stuff "pardon {0} ^M"'.format(x)
                 self.logWriter.write("[{1}][checkUsers] Trying to unban with string: {0}.\n".format(unbanString, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
                 call(unbanString, shell=True)
                 self.logWriter.write("[{3}][checkUsers] Removed ban and added lives for {0} with UUID {1}.\n".format(x, uuid, etime, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
-        self.logWriter.write("[{1}][checkUsers] Finished checkUsers. Current dictionary {0}.\n".format(BannedPurger.bDict, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+        self.logWriter.write("[{1}][checkUsers] Finished checkUsers. Current dictionary {0}.\n".format(self.bDict, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+        del self.bDict
 
 
 # Running part
